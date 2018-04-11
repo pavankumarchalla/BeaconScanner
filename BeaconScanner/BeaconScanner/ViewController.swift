0f8fa012bf02d76a671c877ce86540c8a6a767cd
Pavan Kumar C <pavan.challa23@gmail.com>
2018-04-11 23:54:03 +0530

    single view app created

---
+++ BeaconScanner/BeaconScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeaconScanner
+//
+//  Created by Pavan Kumar C on 11/04/18.
+//  Copyright © 2018 pavan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
