5c30dfb52b05cee7f8c3289b2ac12089ec6dab7e
Pavan Kumar C <pavan.challa23@gmail.com>
2018-04-12 01:47:40 +0530

    Eddystone and ripple effect configured

--- BeaconScanner/BeaconScanner/ViewController.swift
+++ BeaconScanner/BeaconScanner/ViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, BeaconScannerDelegate {
+
+  @IBOutlet weak var baseView: UIView!
+  @IBOutlet weak var scanButton: UIButton!
+  
+  var rippleView: SMRippleView?
+  var beaconScanner: BeaconScanner!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    baseView.isHidden = true
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
 
-
+  //MARK:- IBAction Methods
+  
+  @IBAction func scanForBeaconsClicked(_ sender: UIButton) {
+    scanButton.isHidden = true
+    baseView.isHidden = false 
+    let fillColor: UIColor? = UIColor(red: 255/255, green: 171/255, blue: 73/255, alpha: 1)
+    rippleView = SMRippleView(frame: baseView.bounds, rippleColor: UIColor.black, rippleThickness: 0.2, rippleTimer: 0.6, fillColor: fillColor, animationDuration: 4, parentFrame: self.view.frame)
+    self.baseView.addSubview(rippleView!)
+    
+    self.beaconScanner = BeaconScanner()
+    self.beaconScanner!.delegate = self
+    self.beaconScanner!.startScanning()
+  }
+  
+  //MARK:- Beacon Scanner Delegate
+  
+  func didFindBeacon(beaconScanner: BeaconScanner, beaconInfo: BeaconInfo) {
+    print("FIND: \(beaconInfo.description)")
+  }
+  
+  func didLoseBeacon(beaconScanner: BeaconScanner, beaconInfo: BeaconInfo) {
+    print("LOST: \(beaconInfo.description)")
+  }
+  
+  func didUpdateBeacon(beaconScanner: BeaconScanner, beaconInfo: BeaconInfo) {
+    print("UPDATE: \(beaconInfo.description)")
+  }
+  
+  func didObserveURLBeacon(beaconScanner: BeaconScanner, URL: NSURL, RSSI: Int) {
+    showAlert("Beacon Found", message: "URL SEEN: \(URL)")
+  }
+  
+  //MARK:- Custom methods
+  
+  func showAlert(_ title:String, message: String) {
+    DispatchQueue.main.async {
+      self.beaconScanner.stopScanning()
+      self.baseView.isHidden = true
+      let alert = UIAlertController(title: title, message:message, preferredStyle: UIAlertControllerStyle.alert)
+      alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {(action:UIAlertAction) in
+        self.scanButton.isHidden = false
+      }));
+      self.present(alert, animated: true, completion: nil)
+    }
+  }
+  
 }
 
